7f8ad8a394d4fe44760eb7ef69fc40212a11381e
cindylasco127 <101797135+cindylasco127@users.noreply.github.com>
2022-06-18 13:12:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ st.markdown("Prediction of the Banana Diseases: Bunchy Top, Moko, Sigatoka and F
 
 def main():
     file_uploaded = st.file_uploader("Select Image", type=["png","jpg","jpeg"])
-    class_btn = st.button("Classify")
     if file_uploaded is not None:    
         image = Image.open(file_uploaded)
         st.image(image, caption='Uploaded Image', use_column_width=True)
-        
+    
+    class_btn = st.button("Classify")
     if class_btn:
         if file_uploaded is None:
             st.write("Invalid command, please upload an image")
